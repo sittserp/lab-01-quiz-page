d6f0aca0a72e8658147501f641f57c66f616517b
Perry Sittser <sittserp@gmail.com>
2020-09-28 15:14:12 -0700

    quiz questions and functionality

--- app.js
+++ app.js
@@ -6,7 +6,26 @@ const score = document.getElementById('score');
 
 // initialize state
 
-let score = 0;
+let tally = 0;
 
 // set event listeners to update state and DOM
+
+button.addEventListener('click', () => {
+    const userConfirmed = confirm('Do you really want to take the quiz?');
+        if (!userConfirmed) {
+            alert('You chose wisely.');
+            return;
+        }
+    const userName = prompt('What is your name?');
+    const habitat = prompt(`Hi, ${userName}!  Does the coconut octopus live in the Pacific Ocean?`);
+        if (countAsYes(habitat)) {tally++};
+    const movement = prompt('Does the coconut octopus walk on two legs in the water?');
+        if (countAsYes(movement)) {tally++};
+    const tree = prompt('Does the coconut octopus live in coconut trees?');
+        if (!countAsYes(tree)) {tally++}
+    const food = prompt(`Okay, ${userName}, last one: Does the coconut octopus eat shrimp?`);
+        if (countAsYes(food)) {tally++};
+    score.textContent = `Congratulations, ${userName}! You got ${tally} out of 4 correct!`;
+    score.scrollTop = score.scrollHeight;
+});
 
