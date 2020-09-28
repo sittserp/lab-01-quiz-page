b1f1193ca53b42bef1079639aa2bb60a2ff1d011
Perry Sittser <sittserp@gmail.com>
2020-09-28 16:41:45 -0700

    different messages for different scores

--- app.js
+++ app.js
@@ -30,9 +30,23 @@ button.addEventListener('click', () => {
     const food = prompt(`Okay, ${userName}, last one: Does the coconut octopus eat shrimp?`);
         if (countAsYes(food)) {tally++};
 
-    alert('Well done!  Check your score at the bottom of the page!')
+    alert('Well done!  Check your score at the bottom of the page!');
 
-    score.textContent = `Congratulations, ${userName}! You got ${tally} out of 4 correct!`;
+    tally = tally / 4 * 100;
+
+    if (tally === 0) {score.textContent = `Sorry, ${userName}, you got ${tally}% correct. Try again!`};
+
+    if (tally === 25) {score.textContent = `Oooooo... sorry, ${userName}, you only got ${tally}% correct.  Try again!`};
+
+    if (tally === 75) {score.textContent = `Not bad, ${userName}!  You scored ${tally}%.  Try again to get them all right!`};
+
+    if (tally === 100) {score.textContent = `Great job, ${userName}!  You got ${tally}% correct!`};
+
+    // score.textContent = `Congratulations, ${userName}! You got ${tally}% correct!`;
+
+    console.log(tally);
+
+    tally = 0;
 
 });
 
