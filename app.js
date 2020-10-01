77733de0cdbb273ea3a7b82093f83c4a92ef4ac7
Perry Sittser <sittserp@gmail.com>
2020-09-30 18:45:01 -0700

    fix eslint issues

--- app.js
+++ app.js
@@ -12,35 +12,35 @@ let tally = 0;
 
 button.addEventListener('click', () => {
     const userConfirmed = confirm('Do you really want to take the quiz?');
-        if (!userConfirmed) {
-            alert('You chose wisely.');
-            return;
-        }
+    if (!userConfirmed) {
+        alert('You chose wisely.');
+        return;
+    }
     const userName = prompt('What is your name?');
 
     const habitat = prompt(`Hi, ${userName}!  Does the coconut octopus live in the Pacific Ocean?`);
-        if (countAsYes(habitat)) {tally++};
+    if (countAsYes(habitat)) {tally++;}
 
     const movement = prompt('Does the coconut octopus walk on two legs in the water?');
-        if (countAsYes(movement)) {tally++};
+    if (countAsYes(movement)) {tally++;}
 
     const tree = prompt('Does the coconut octopus live in coconut trees?');
-        if (!countAsYes(tree)) {tally++}
+    if (!countAsYes(tree)) {tally++;}
 
     const food = prompt(`Okay, ${userName}, last one: Does the coconut octopus eat shrimp?`);
-        if (countAsYes(food)) {tally++};
+    if (countAsYes(food)) {tally++;}
 
     alert('Well done!  Check your score at the bottom of the page!');
 
     tally = tally / 4 * 100;
 
-    if (tally === 0) {score.textContent = `Sorry, ${userName}, you got ${tally}% correct. Try again!`};
+    if (tally === 0) {score.textContent = `Sorry, ${userName}, you got ${tally}% correct. Try again!`;}
 
-    if (tally === 25) {score.textContent = `Oooooo... sorry, ${userName}, you only got ${tally}% correct.  Try again!`};
+    if (tally === 25) {score.textContent = `Oooooo... sorry, ${userName}, you only got ${tally}% correct.  Try again!`;}
 
-    if (tally === 75) {score.textContent = `Not bad, ${userName}!  You scored ${tally}%.  Try again to get them all right!`};
+    if (tally === 75) {score.textContent = `Not bad, ${userName}!  You scored ${tally}%.  Try again to get them all right!`;}
 
-    if (tally === 100) {score.textContent = `Great job, ${userName}!  You got ${tally}% correct!`};
+    if (tally === 100) {score.textContent = `Great job, ${userName}!  You got ${tally}% correct!`;}
 
     // score.textContent = `Congratulations, ${userName}! You got ${tally}% correct!`;
 
